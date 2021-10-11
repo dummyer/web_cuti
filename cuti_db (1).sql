9da506e12dc1143766fa851a2eac9c25b7c486d9
Carina Natalia <31266534+Dummyer@users.noreply.github.com>
2021-10-11 17:31:30 +0700

    update

--- cuti_db (1).sql
+++ cuti_db (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2021 at 11:40 AM
+-- Generation Time: Oct 11, 2021 at 12:31 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.4.20
 
